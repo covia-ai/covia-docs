31ffa7833ce42d9d1b6bee96323bd9dbba96a74b
mikera <mike@mikera.net>
2025-06-23 14:22:24 +0100

    Update README

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://covia.ai',
+  url: 'https://docs.covia.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
